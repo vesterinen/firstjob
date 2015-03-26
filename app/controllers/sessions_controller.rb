3d055d02523527f0fd253c8be78db847c3b6f3b0
github username <github email address>
2015-03-26 19:17:44 -0400

    working match_generator

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create    
+    if @user = User.login(params[:email])
+      login(@user)
+      if session[:back_to].present?
+        back_to = session[:back_to]
+        session[:back_to] = nil
+        redirect_to back_to
+      else
+        redirect_to root_path
+      end
+    else
+      flash.now[:notice] = "Ooops! Looks like you have not registered yet."
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
+end
