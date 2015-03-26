7edb9b75c667deb2873e0be03a29efa1fba923bb
github username <github email address>
2015-03-26 16:31:45 -0400

    working schema

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,37 +1,5 @@
 class MatchesController < ApplicationController
 
-  def index
-    matches = Match.all
-  end
-
-  def new
-    match = Match.new
-  end
-
-  def show
-    match = Match.find(params[:id])
-  end
-
-  def edit
-    match = Match.find(params[:id])
-  end
-
-  def update
-    match = Match.find(params[:id])
-    match.update(match_params)
-    redirect_to match_path(match)
-  end
-
-  def create
-    match = Match.new(match_params)
-    match.save
-  end
-
-
-private
-  def match_params
-    params.require(:match).permit(:status)
-  end
-
-
+#we need to match a user with 1-3 potential mentors/mentees
+#if user is a student, find mentor(s) where location (&industry) is the same -- and vice versa
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,13 @@
 class Match < ActiveRecord::Base
   belongs_to :mentor, class_name: "User"
   belongs_to :student, class_name: "User"
+
+  # def match(user, location)
+  #   if user.role == "student"
+  #     user.mentor << User.where(location: location).limit(3)
+  #   end
+  # end
+
 end
 
 
