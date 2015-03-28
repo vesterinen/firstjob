d18895e12bbd00ca9c6f9d4810e857f796da9a91
Ville Vesterinen <villevesterinen@gmail.com>
2015-03-27 20:09:39 -0400

    add profile tab in the navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def login_or_logout
     if logged_in?
-      "Logged in as: #{current_user.email} #{link_to "Logout", logout_path}".html_safe
+      " #{link_to "Logout", logout_path}".html_safe
     else
       link_to "Login", login_path
     end
-  end  
+  end
 end
