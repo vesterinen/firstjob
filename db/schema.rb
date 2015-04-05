800628d05e11a8497d9a2099ffb84a10b3d29fdf
github username <github email address>
2015-04-05 13:32:10 -0400

    added languages as an additional filter

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330235214) do
+ActiveRecord::Schema.define(version: 20150405173102) do
 
   create_table "industries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -34,6 +40,16 @@ ActiveRecord::Schema.define(version: 20150330235214) do
 
   add_index "user_industries", ["industry_id"], name: "index_user_industries_on_industry_id"
   add_index "user_industries", ["user_id"], name: "index_user_industries_on_user_id"
+
+  create_table "user_languages", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "language_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "user_languages", ["language_id"], name: "index_user_languages_on_language_id"
+  add_index "user_languages", ["user_id"], name: "index_user_languages_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
